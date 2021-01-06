fb6b076da335da5dc6e104e155e41feee0150245
Avshalom <avshalom.tam@gmail.com>
2021-01-06 16:41:40 +0200

    fixed a bug

--- public/js/search.js
+++ public/js/search.js
@@ -4,6 +4,11 @@ const orderTwo = document.querySelector('#order-2');
 const orderThree = document.querySelector('#order-3');
 const orderFour = document.querySelector('#order-4');
 const orderFive = document.querySelector('#order-5');
+const orderSix = document.querySelector('#order-6');
+const orderSeven = document.querySelector('#order-7');
+const orderEight = document.querySelector('#order-8');
+const orderNine = document.querySelector('#order-9');
+const orderTen = document.querySelector('#order-10');
 
 ordersButton.addEventListener('click', (e) => {
     e.preventDefault()
@@ -12,6 +17,11 @@ ordersButton.addEventListener('click', (e) => {
     orderThree.textContent = '';
     orderFour.textContent = '';
     orderFive.textContent = '';
+    orderSix.textContent = '';
+    orderSeven.textContent = '';
+    orderEight.textContent = '';
+    orderNine.textContent = '';
+    orderTen.textContent = '';
 
     fetch('/orders').then((response) => {
         response.json().then((data) => {
@@ -20,6 +30,11 @@ ordersButton.addEventListener('click', (e) => {
             orderThree.textContent = data.orderThree
             orderFour.textContent = data.orderFour
             orderFive.textContent = data.orderFive
+            orderSix.textContent = data.orderSix
+            orderSeven.textContent = data.orderSeven
+            orderEight.textContent = data.orderEight
+            orderNine.textContent = data.orderNine
+            orderTen.textContent = data.orderTen
         })
     })
 });
--- utils/organizeMsg.js
+++ utils/organizeMsg.js
@@ -1,17 +1,27 @@
-const organizeMsg = (orderOne='', orderTwo='', orderThree='', orderFour='', orderFive='') => {
+const organizeMsg = (orderOne='', orderTwo='', orderThree='', orderFour='', orderFive='', orderSix='', orderSeven='', orderEight='', orderNine='', orderTen='') => {
     const validMsg = {
         orderOne,
         orderTwo,
         orderThree,
         orderFour,
-        orderFive
+        orderFive,
+        orderSix,
+        orderSeven,
+        orderEight,
+        orderNine,
+        orderTen
     }
     return {
         orderOne: validMsg.orderOne ?  sentense(validMsg.orderOne) : '',
         orderTwo: validMsg.orderTwo ? sentense(validMsg.orderTwo) : '',
         orderThree: validMsg.orderThree ? sentense(validMsg.orderThree) : '',
         orderFour: validMsg.orderFour ? sentense(validMsg.orderFour) : '',
-        orderFive: validMsg.orderFive ? sentense(validMsg.orderFive) : ''
+        orderFive: validMsg.orderFive ? sentense(validMsg.orderFive) : '',
+        orderSix: validMsg.orderSix ?  sentense(validMsg.orderSix) : '',
+        orderSeven: validMsg.orderSeven ? sentense(validMsg.orderSeven) : '',
+        orderEight: validMsg.orderEight ? sentense(validMsg.orderEight) : '',
+        orderNine: validMsg.orderNine ? sentense(validMsg.orderNine) : '',
+        orderTen: validMsg.orderTen ? sentense(validMsg.orderTen) : ''
     }
 };
 
